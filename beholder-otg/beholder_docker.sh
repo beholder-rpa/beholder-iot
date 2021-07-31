c9b5e880cb5c0abd9e3984723cf1c27e2f6e95a2
Sean McLellan <sean@baristalabs.io>
2021-07-31 16:37:40 -0400

    Fix service

--- beholder-otg/beholder_docker.sh
+++ beholder-otg/beholder_docker.sh
@@ -4,13 +4,13 @@ echo "# Executing Beholder IoT Docker script..."
 
 if [ "$1" = up ]
 then
-    ./beholder.ps1 build rpi
-    ./beholder.ps1 up rpi
+    ~/beholder/beholder.ps1 build rpi
+    ~/beholder/beholder.ps1 up rpi
 fi
 
 if [ "$1" = down ]
 then
-    ./beholder.ps1 down rpi
+    ~/beholder/beholder.ps1 down rpi
 fi
 
 echo "# Completed Beholder IoT Docker script."
