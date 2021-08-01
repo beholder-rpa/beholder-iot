2242c9f2b73b88bc8445b19bc8c35dce3d72a89e
Sean McLellan <sean@baristalabs.io>
2021-08-01 15:33:20 -0400

    Attempt something else for arm7 builds

--- beholder-stalk/docker-restore-xplat.sh
+++ beholder-stalk/docker-restore-xplat.sh
@@ -3,6 +3,11 @@ if [ "$TARGETPLATFORM" = "linux/arm64" ]; then
     dotnet restore "./beholder-stalk.csproj" --runtime linux-arm64
 elif [ "$TARGETPLATFORM" = "linux/arm/v7" ]; then
     echo "Targeting linux/arm/v7";
+    # Sigh... Just microsoft things...
+    # https://docs.microsoft.com/en-us/dotnet/core/install/linux-package-mixup
+    sudo dnf remove packages-microsoft-prod
+    sudo dnf remove 'dotnet*' 'aspnet*' 'netstandard*'
+    sudo dnf install dotnet-sdk-5.0
     dotnet restore "./beholder-stalk.csproj" --runtime linux-arm
 elif [ "$TARGETPLATFORM" = "linux/amd64" ]; then
     echo "Targeting linux/amd64";
