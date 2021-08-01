160a1f3f9f6ad9c6e3d3c6ac7f292623b148a6c5
Sean McLellan <sean@baristalabs.io>
2021-08-01 14:52:08 -0400

    Attempt to specify platform on restore as well

---
+++ beholder-stalk/docker-restore-xplat.sh
@@ -0,0 +1,13 @@
+if [ "${TARGETPLATFORM}" = "linux/arm64" ]; then
+    echo "Targeting linux/arm64";
+    dotnet restore "./beholder-stalk.csproj" --runtime linux-arm64
+elif [ "${TARGETPLATFORM}" = "linux/arm/v7" ]; then
+    echo "Targeting linux/arm/v7";
+    dotnet restore "./beholder-stalk.csproj" --runtime linux-arm
+elif [ "${TARGETPLATFORM}" = "linux/amd64" ]; then
+    echo "Targeting linux/amd64";
+    dotnet restore "./beholder-stalk.csproj" --runtime linux-x64
+else then
+    echo "Targeting generic platform";
+    dotnet restore "./beholder-stalk.csproj" -o /app/build
+fi
