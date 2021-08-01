9c0a63d3e815e9e1f2aebac442a21b862151d28f
Sean McLellan <sean@baristalabs.io>
2021-08-01 17:02:28 -0400

    Another attempt

--- beholder-stalk/docker-restore-xplat.sh
+++ beholder-stalk/docker-restore-xplat.sh
@@ -5,18 +5,10 @@ elif [ "$TARGETPLATFORM" = "linux/arm/v7" ]; then
     echo "Targeting linux/arm/v7";
     # Sigh... Just microsoft things...
     # https://docs.microsoft.com/en-us/dotnet/core/install/linux-package-mixup
-    wget https://download.visualstudio.microsoft.com/download/pr/f456f253-db24-45ea-9c73-f507f93a8cd2/6efe7bed8639344d9c9afb8a46686c99/dotnet-sdk-5.0.302-linux-arm.tar.gz
-    wget https://download.visualstudio.microsoft.com/download/pr/7e928c60-5f60-4c62-8439-422be547605c/0d1dc316cf38efdb2557f639ca9da4ad/aspnetcore-runtime-5.0.8-linux-arm.tar.gz
-    rm /usr/bin/dotnet
-    rm -rf /usr/share/dotnet
-    mkdir /usr/share/dotnet
-    tar -xvf dotnet-sdk-5.0.302-linux-arm.tar.gz -C /usr/share/dotnet
-    tar -xvf aspnetcore-runtime-5.0.8-linux-arm.tar.gz -C /usr/share/dotnet
-    ln -s /usr/share/dotnet/dotnet /usr/bin/dotnet
-    export DOTNET_ROOT=/usr/share/dotnet/
-    ls -l /usr/share/dotnet/host/fxr
-    ls -l /usr/share/dotnet/host/fxr/5.0.8
-    dotnet --version
+    # "A fatal error occurred, the folder [/usr/share/dotnet/host/fxr] does not contain any version-numbered child folders" error will be reported.
+    # This is also really useful: https://github.com/dotnet/dotnet-docker/issues/1537#issuecomment-615269150
+    # You can not run .NET within arm32 images on amd64 hosts but it is possible to build an .NET arm32 image on an amd64 host using the [this pattern](https://github.com/dotnet/dotnet-docker/blob/master/samples/dotnetapp/Dockerfile.debian-arm32).
+    # The subtle difference here is you are using an amd64 SDK image to product the arm32 binaries that get copied to the resulting arm32 image.
     dotnet restore "./beholder-stalk.csproj" --runtime linux-arm
 elif [ "$TARGETPLATFORM" = "linux/amd64" ]; then
     echo "Targeting linux/amd64";
