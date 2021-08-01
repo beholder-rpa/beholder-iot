72d4ee93a567cf073d5f3d6208b7eb4d83368487
Sean McLellan <sean@baristalabs.io>
2021-08-01 03:08:58 -0400

    fix thing

--- beholder-traefik/certs/makecerts.ps1
+++ beholder-traefik/certs/makecerts.ps1
@@ -30,7 +30,7 @@ if (Get-Command "openssl" -ErrorAction SilentlyContinue)
 
         if (-not(Test-Path -Path "$outputPath/$domain.crt" -PathType Leaf)) {
 
-            & openssl req -key "$outputPath/server.key" -x509 -sha256 -nodes -days 1825`
+            & openssl req -key "$outputPath/server.key" -x509 -sha256 -nodes -days 1825 `
                 -subj "/C=US/ST=Oregon/L=Portland/O=Beholder/OU=Beholder/CN=$domain" `
                 -addext "subjectAltName=DNS:$domain" `
                 -out "$outputPath/$domain.crt" `
