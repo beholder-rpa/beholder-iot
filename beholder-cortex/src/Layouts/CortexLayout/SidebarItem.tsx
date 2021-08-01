e96a804c3b72c1b7f8fed3e28d3aa0f2b21273ac
Sean McLellan <sean@baristalabs.io>
2021-08-01 12:02:12 -0400

    Highlight the acctive item

--- beholder-cortex/src/Layouts/CortexLayout/SidebarItem.tsx
+++ beholder-cortex/src/Layouts/CortexLayout/SidebarItem.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 import classNames from '@src/utils/classNames';
 
@@ -10,18 +11,26 @@ type Props = {
   className?: string;
 };
 
-const SidebarItem = ({ title = 'Title', icon, url, className }: Props) => (
-  <Link href={url} passHref={true}>
-    <a
-      className={classNames(
-        'relative flex flex-row items-center h-11 hover:bg-skin-600 dark:hover:bg-skin-700 text-gray-200 hover:text-gray-100 border-l-4 border-transparent hover:border-skin-200 dark:hover:border-skin-400 pr-6',
-        className,
-      )}
-    >
-      <span className="inline-flex justify-center items-center ml-3">{icon}</span>
-      <span className="ml-2 text-sm tracking-wide truncate">{title}</span>
-    </a>
-  </Link>
-);
+const SidebarItem = ({ title = 'Title', icon, url, className }: Props) => {
+  const router = useRouter();
+  let highlight = '';
+  if (router.asPath === url) {
+    highlight = 'border-skin-300 bg-base-300';
+  }
+  return (
+    <Link href={url} passHref={true}>
+      <a
+        className={classNames(
+          'relative flex flex-row items-center h-11 hover:bg-skin-600 dark:hover:bg-skin-700 text-gray-200 hover:text-gray-100 border-l-4 border-transparent hover:border-skin-200 dark:hover:border-skin-400 pr-6',
+          className,
+          highlight,
+        )}
+      >
+        <span className="inline-flex justify-center items-center ml-3">{icon}</span>
+        <span className="ml-2 text-sm tracking-wide truncate">{title}</span>
+      </a>
+    </Link>
+  );
+};
 
 export default SidebarItem;
