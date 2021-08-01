edf68914425515240b317fa6c8cd2292bd04a67d
Sean McLellan <sean@baristalabs.io>
2021-08-01 15:46:52 -0400

    More build on arm7 fun

--- beholder-stalk/docker-restore-xplat.sh
+++ beholder-stalk/docker-restore-xplat.sh
@@ -5,9 +5,16 @@ elif [ "$TARGETPLATFORM" = "linux/arm/v7" ]; then
     echo "Targeting linux/arm/v7";
     # Sigh... Just microsoft things...
     # https://docs.microsoft.com/en-us/dotnet/core/install/linux-package-mixup
-    sudo dnf remove packages-microsoft-prod
-    sudo dnf remove 'dotnet*' 'aspnet*' 'netstandard*'
-    sudo dnf install dotnet-sdk-5.0
+    apt-get remove -y packages-microsoft-prod
+    apt-get remove -y 'dotnet*' 'aspnet*' 'netstandard*'
+    apt-get install -y wget
+    wget https://packages.microsoft.com/config/debian/10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+    dpkg -i packages-microsoft-prod.deb
+    rm packages-microsoft-prod.deb
+    apt-get update; \
+        apt-get install -y apt-transport-https && \
+        apt-get update && \
+        apt-get install -y dotnet-sdk-5.0
     dotnet restore "./beholder-stalk.csproj" --runtime linux-arm
 elif [ "$TARGETPLATFORM" = "linux/amd64" ]; then
     echo "Targeting linux/amd64";
