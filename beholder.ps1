ab1c75226ac7fb3882094c8e6225c57cd427e180
Sean McLellan <sean@baristalabs.io>
2021-08-11 23:37:34 -0400

    Tweaks

--- beholder.ps1
+++ beholder.ps1
@@ -18,6 +18,7 @@ $env:BEHOLDER_SHORT_HOSTNAME = $hostName.Split(".")[0]
 
 if ($environment -eq "rpi") {  
   $env:BEHOLDER_CORTEX_HOSTNAME = $hostName
+  $env:BEHOLDER_TRAEFIK_HOSTNAME = "traefik.$hostName"
   $env:BEHOLDER_NEXUS_HOSTNAME = "nexus.$hostName"
   $env:BEHOLDER_GRAFANA_HOSTNAME = "grafana.$hostName"
   $env:BEHOLDER_JAEGER_HOSTNAME = "jaeger.$hostName"
