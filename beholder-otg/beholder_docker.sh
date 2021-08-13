e071a97b189e30884575993ec25fea38b093c1c4
Sean McLellan <sean@baristalabs.io>
2021-08-12 21:55:22 -0400

    Fix systemctl

--- beholder-otg/beholder_docker.sh
+++ beholder-otg/beholder_docker.sh
@@ -4,11 +4,11 @@ echo "# Executing Beholder IoT Docker script..."
 
 if [ "$1" = up ]
 then
-    export SHORT_HOST=$(echo $HOSTNAME | sed -En 's/^(.*?)\.local$/\1/p')
-    sudo systemctl enable --now avahi-alias@cerebrum.$SHORT_HOST.service
-    sudo systemctl enable --now avahi-alias@traefik.$SHORT_HOST.service
-    sudo systemctl enable --now avahi-alias@nexus.$SHORT_HOST.service
-    sudo systemctl enable --now avahi-alias@grafana.$SHORT_HOST.service
+    #export SHORT_HOST=$(echo $HOSTNAME | sed -En 's/^(.*?)\.local$/\1/p')
+    sudo systemctl enable --now avahi-alias@"cerebrum.$HOSTNAME".service
+    sudo systemctl enable --now avahi-alias@"traefik.$HOSTNAME".service
+    sudo systemctl enable --now avahi-alias@"nexus.$HOSTNAME".service
+    sudo systemctl enable --now avahi-alias@"grafana.$HOSTNAME".service
 
     pushd /home/beholder/beholder/
     git pull --depth 1
