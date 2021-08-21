2a92a8ce6ebb2b249a65007fdfb5793eeb2ca635
Sean McLellan <sean@baristalabs.io>
2021-08-21 01:01:17 -0400

    rely on platform detection

--- beholder-otg/beholder_docker.sh
+++ beholder-otg/beholder_docker.sh
@@ -12,9 +12,9 @@ then
     pushd /home/beholder/beholder/
     git reset origin --hard
     git pull --depth 1
-    ./beholder.ps1 pull rpi
-    ./beholder.ps1 build rpi
-    ./beholder.ps1 up rpi
+    ./beholder.ps1 pull
+    ./beholder.ps1 build
+    ./beholder.ps1 up
     popd
     chown -R pi /home/beholder/beholder/
 fi
@@ -22,7 +22,7 @@ fi
 if [ "$1" = down ]
 then
     pushd /home/beholder/beholder/
-    ./beholder.ps1 down rpi
+    ./beholder.ps1 down
     popd
 
     #export SHORT_HOST=$(echo $HOSTNAME | sed -En 's/^(.*?)\.local$/\1/p')
