92ec170a975eba45b22aed44ee494f0dde0a1d12
Sean McLellan <sean@baristalabs.io>
2021-08-27 13:27:37 -0400

    Allow empty hostname on unregister as well

--- beholder-cerebrum/data/node-red-contrib-beholder/nodes/psionix/register-hotkey/register-hotkey.js
+++ beholder-cerebrum/data/node-red-contrib-beholder/nodes/psionix/register-hotkey/register-hotkey.js
@@ -11,18 +11,20 @@ module.exports = function (RED) {
       } else {
         body = `${config.modifiers}${config.key}`;
       }
+
       let hostName = config.hostname;
       if (!hostName) {
         const beholderServices = globalContext.get('beholder_services');
-        if (beholderServices && beholderServices.psionix) {
+        if (beholderServices && beholderServices.daemon) {
           hostName = beholderServices.psionix[0];
         }
 
         if (!hostName) {
-          node.error('No psionix hostname specified and a psionix hostname could not be determined from the beholder_services global');
+          node.error('No daemon hostname specified and a daemon hostname could not be determined from the beholder_services global');
           return;
         }
       }
+      
       this.send({
         topic: `beholder/psionix/${hostName}/hotkeys/register`,
         payload: body
--- beholder-cerebrum/data/node-red-contrib-beholder/nodes/psionix/unregister-hotkey/unregister-hotkey.js
+++ beholder-cerebrum/data/node-red-contrib-beholder/nodes/psionix/unregister-hotkey/unregister-hotkey.js
@@ -9,6 +9,20 @@ module.exports = function (RED) {
       } else {
         body = `${config.modifiers}${config.key}`
       }
+      
+      let hostName = config.hostname;
+      if (!hostName) {
+        const beholderServices = globalContext.get('beholder_services');
+        if (beholderServices && beholderServices.daemon) {
+          hostName = beholderServices.psionix[0];
+        }
+
+        if (!hostName) {
+          node.error('No daemon hostname specified and a daemon hostname could not be determined from the beholder_services global');
+          return;
+        }
+      }
+
       this.send({
         topic: `beholder/psionix/${config.hostname}/hotkeys/unregister`,
         payload: body
